4646abc67e9c716fc0d1b003f54504d3fc4baf73
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-05-03 12:05:39 +0600

    Usecontext added

---
+++ Contexts/UserContext/UserContext.js
@@ -0,0 +1,75 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {  createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile,  } from 'firebase/auth'
+import app from '../../src/Component/Firebase/firebase.config';
+
+// Export Authcontext
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const UserContext = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Google / Github provider.
+    const providerLogin = (provider) => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    }
+// For  a new user 
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+// Login with mail
+    const logIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile);
+    }
+
+    
+//Log out 
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+// For check the statement
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('inside auth state change', currentUser);
+
+            if(currentUser === null || currentUser.emailVerified){
+                setUser(currentUser);
+            }
+            setLoading(false);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+
+    }, [])
+
+
+    const authInfo = { 
+        user, 
+        loading, 
+        setLoading,
+        providerLogin, 
+        logOut, 
+        updateUserProfile,
+        createUser, 
+        logIn 
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
